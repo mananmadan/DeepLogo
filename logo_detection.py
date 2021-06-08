1d2374d0aa28a364de2c5ac9e077877a1b0ce8c2
mananmadan <madan.manan1@gmail.com>
2021-06-08 12:57:08 +0530

    auto update

--- logo_detection.py
+++ logo_detection.py
@@ -121,6 +121,7 @@ if __name__ == "__main__":
     image = Image.open(image_path)
         # the array based representation of the image will be used later in order to prepare the
         # result image with boxes and labels on it.
+    print(image.shape)
     image_np = load_image_into_numpy_array(image)
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
