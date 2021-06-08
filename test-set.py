891e3f2479fd5e3fdd28da42738bb857dc163c26
mananmadan <madan.manan1@gmail.com>
2021-06-08 19:21:49 +0530

    auto update

--- test-set.py
+++ test-set.py
@@ -142,8 +142,8 @@ if __name__ == "__main__":
         if output_dict['detection_scores'][j] > 0.5:
             detected_class = category_index[output_dict['detection_classes'][j]]['name']
             print("detected_class",detected_class)
+            print("desired class",category_index[int(cat[cnt])]['name'])
             if detected_class == category_index[int(cat[cnt])]['name']:
-                print("desired class",category_index[cat[cnt]]['name'])
                 correct = correct + 1
             break
     print("done cnt:",cnt,"correct count",correct)
