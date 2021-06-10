d8ee93bf80b04a04097bb8306148e9e0f2ea0a36
mananmadan <madan.manan1@gmail.com>
2021-06-10 11:05:11 +0530

    auto update

--- logo_detection.py
+++ logo_detection.py
@@ -138,21 +138,15 @@ if __name__ == "__main__":
         # Actual detection.
     output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
         # Visualization of the results of a detection.
-    cnt = 0;
+    cnt = 0
+    mx = 0
+    detected = ""
     for j in output_dict['detection_classes']:
-        if output_dict['detection_scores'][cnt] > 0.5:
-            print(category_index[j]['name'])
+        if output_dict['detection_scores'][cnt] > mx:
+            mx = output_dict['detection_Scores'][cnt]
+            detected = category_index[j]['name']
         cnt = cnt + 1
-
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
+    print(detected)
     plt.figure(figsize=IMAGE_SIZE)
     plt.imshow(image_np)
     plt.savefig(os.path.join(args.output_dir, 'detect_results_' + i))
