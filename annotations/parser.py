092df95da00e71e7c308e7e5d5e84ce504ba21f5
mananmadan <madan.manan1@gmail.com>
2021-06-14 17:42:19 +0530

    ISSUE RESOLVED:Removed problem of duplicate imgs in annot

--- annotations/parser.py
+++ annotations/parser.py
@@ -8,30 +8,53 @@ test_file  = 'flickr_logos_27_dataset_test_set_annotation_cropped.txt'
 ## Load data from the annotation file
 trdata = np.loadtxt(train_file, delimiter=',', dtype=str)
 trclasses = trdata[:, -1] # last col has data regarding the classes
-
+imgtr = trdata[:,0]
 tedata = np.loadtxt(test_file,delimiter=',',dtype=str)
 teclasses = tedata[:, -1]
+imgte = tedata[:,0]
 
 
 ## Load the list mapping index to name
 cat_index = ["Adidas","Apple","BMW","Citroen","Cocacola","DHL","Fedex","Ferrari","Ford","Google","HP","Heineken","Intel","McDonalds","Mini","Nbc","Nike","Pepsi","Porsche","Puma","RedBull","Sprite","Starbucks","Texaco","Unicef","Vodafone","Yahoo"]
-
+cnt = 0
+## avoid recounting
+vis = {}
 
 ## Count the distribution
 trdict = {}
 tedict = {}
-for i in trclasses:
-   if int(i) not in trdict:
-	   trdict[int(i)] = 1
+for i in range(0,len(trclasses)):
+   cindex = int(trclasses[i])
+   name = imgtr[i]
+   if name in vis:
+	   continue
+   vis[name] = 1
+   if int(cindex) not in trdict:
+	   trdict[cindex] = 1
    else:
-	   trdict[int(i)] = trdict[int(i)]+1
+	   trdict[cindex] = trdict[cindex]+1
 
-for i in teclasses:
-   if int(i) not in tedict:
-	   tedict[int(i)] = 1
+for i in range(0,len(teclasses)):
+   cindex = int(teclasses[i])
+   name = imgte[i]
+   if name in vis:
+	   continue
+   vis[name] = 1
+   if cindex not in tedict:
+	   tedict[cindex] = 1
    else:
-	   tedict[int(i)] = tedict[int(i)]+1
+	   tedict[cindex] = tedict[cindex]+1
 
+## Print freq by name
+sum = 0
+for i in trdict:
+	sum = sum + trdict[i]
+	print(cat_index[i],":",trdict[i])
+print("Total training images:",sum)
+sum = 0
+for i in tedict:
+	sum  = sum + tedict[i]
+print("Total Testing images:",sum)
 ##Plot the graph
 plt.bar(*zip(*tedict.items()))
 plt.show()
